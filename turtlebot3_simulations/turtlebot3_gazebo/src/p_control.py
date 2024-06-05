6b557061eaa898fbba95bb8304402d0eada2ab68
Arushi <65240373+arushikhokhar@users.noreply.github.com>
2024-06-05 06:30:37 +0100

    Update p_control.py

--- turtlebot3_simulations/turtlebot3_gazebo/src/p_control.py
+++ turtlebot3_simulations/turtlebot3_gazebo/src/p_control.py
@@ -8,23 +8,20 @@ import numpy as np
 
 class Controller:
     def __init__(self) -> None:
-        # Use these Twists to control your robot
         self.move = Twist()
         self.move.linear.x = 0.3
         self.freeze = Twist()
 
-        # The "p" parameter for your p-controller
+        # The "p" parameter of p-controller
         self.angular_vel_coef = 1
 
-        # Create a subscriber for target detection information
         self.target_detection_subscriber = rospy.Subscriber('/target_detection_info', Float32MultiArray, self.target_detection_callback)
         
-        # Create a publisher for your robot "cmd_vel"
         self.cmd_vel_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         
         self.target_detected = False
         self.x_center = 0
-        self.image_width = 640  # Default image width, update as per your camera feed
+        self.image_width = 640 
 
     def target_detection_callback(self, msg):
         if len(msg.data) == 4:
@@ -46,7 +43,7 @@ class Controller:
                 else:
                     self.cmd_vel_publisher.publish(self.freeze)
 
-                rospy.sleep(0.1)  # Loop at 10 Hz
+                rospy.sleep(0.1)  
 
         except rospy.ROSInterruptException:
             pass
